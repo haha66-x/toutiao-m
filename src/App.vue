1857a3aac16dcd6ec30c9db00d4699095c48cead
leiyuhao <1481070775@qq.com>
2021-12-21 19:02:08 +0800

    初始化项目结构

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+   <div>
+     <h2>测试</h2>
+     <router-view> </router-view>
+   </div>
+</template>
+
+<script>
+export default {
+   name:'App'
+}
+</script>
+
+<style lang='less'>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+Vue.use(router);
+const routes=[
+
+],
+const router =new VueRouter({
+     routes
+
+})
